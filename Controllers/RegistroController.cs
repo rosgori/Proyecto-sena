5f9d3464c460f562b11edcc2a2e30fe71f65d508
rosgori <davucor@gmail.com>
2021-09-28 02:54:39 -0500

    Archivos de vistas junto con controladores (sencillos)

---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Proyecto_sena.Models;
+
+namespace Proyecto_sena.Controllers
+{
+    public class RegistroController : Controller
+    {
+        private readonly ILogger<RegistroController> _logger;
+
+        public RegistroController(ILogger<RegistroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
